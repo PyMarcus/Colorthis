4159a5e1e02b2d8f0e3088178c27d4ff7f59f9e7
Marcus <88283829+PyMarcus@users.noreply.github.com>
2023-07-10 00:55:39 -0300

    up constants.go

--- colorthis/constants.go
+++ colorthis/constants.go
@@ -4,13 +4,13 @@ type Color int
 
 const  (
 	Reset Color = 0
-	Red Color = 31
-	Green Color = 32
-	Yellow Color = 33
-	Blue Color = 34
-	Mangeta Color = 35
-	Cyan Color = 36
-	White Color = 37
+	Red Color = 0x1F
+	Green Color = 0x20
+	Yellow Color = 0x21
+	Blue Color = 0x22
+	Mangeta Color = 0x23
+	Cyan Color = 0x24
+	White Color = 0x25
 )
 
 // Base is a function that receive a color and returns the string to print this.
